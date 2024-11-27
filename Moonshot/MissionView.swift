a4147e8ed70a14ce81b78c097bc34a5a18dcfb7a
Constantin Lisnic <constantin@lisnic.dev>
2024-11-27 13:03:09 +0000

    day 41 complete

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Constantin Lisnic on 27/11/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
