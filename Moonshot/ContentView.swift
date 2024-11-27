3abd0a1d00a81393ddb6b7a5f0faeeac59e31445
Constantin Lisnic <constantin@lisnic.dev>
2024-11-27 13:03:43 +0000

    day 41 complete2

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Detailed view for \(mission.displayName)")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
